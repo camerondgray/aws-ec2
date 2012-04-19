71887e0e9d073a770f317843dba4b66afca7ccae
Cameron Gray <cameron.gray@gmail.com>
2012-04-19 11:57:20 -0700

    added test to verify filtering works when describing instances

--- test/integration.js
+++ test/integration.js
@@ -26,7 +26,31 @@ describe('If I issue a request to add a valid ami', function () {
 		it('Should return a list with at least the instance we just launched in it', function (done) {
 			var filters = {},
 				instances = [];
-				aws.getInstances(filters, function (err, response) {
+
+
+			aws.getInstances(filters, function (err, response) {
+				should.not.exist(err);
+				for (var i = 0; i < response.length; i++) {
+					instances.push(response[i].instanceId);
+				}
+				instances.should.not.be.empty;
+				instances.should.include(instanceId);
+				done();
+
+			});
+		});
+
+	});
+	describe('If I request a list of all my currently running instances filtered to the security groups in my config', function () {
+		it('Should return a list with at least the instance we just launched in it', function (done) {
+			var filters = {},
+				instances = [];
+			//{'Filter.1.Name':'private-ip-address', 'Filter.1.Value':privateIp}
+			for (var i = 0; i < config.securityGroups.length; i++) {
+				filters['Filter.' + (i + 1) + '.Name'] = 'group-name';
+				filters['Filter.' + (i + 1) + '.Value'] = config.securityGroups[i];
+			}
+			aws.getInstances(filters, function (err, response) {
 				should.not.exist(err);
 				for (var i = 0; i < response.length; i++) {
 					instances.push(response[i].instanceId);
@@ -56,8 +80,8 @@ describe('If I issue a request to add a valid ami', function () {
 			var instGroups = instance.groupSet.item;
 			var configGroups = config.securityGroups;
 			instGroups.length.should.eql(configGroups.length);
-			//TODO: aws returns the groups in alpha order, if the groups in the config
-			//are not in alpha order this will fail
+			//aws returns the groups in alpha order
+			configGroups = configGroups.sort();
 			for (var i = 0; i < configGroups.length; i++) {
 				configGroups[i].should.eql(instGroups[i].groupName);
 			}
